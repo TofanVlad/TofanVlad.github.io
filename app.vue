a3e38c04f3cc53ad2417d6cb7b680d9f94a5a88f
TofanVlad <VladutuT@gmail.com>
2023-10-29 22:44:28 +0200

    Project init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main
+    class="w-screen h-[100dvh] flex flex-col items-center justify-center bg-zinc-900"
+  ></main>
+</template>
+
+<style scoped></style>
